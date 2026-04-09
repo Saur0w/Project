6deaacc29bcc0acd87b0c08eec4e6a1be6b69109
Saur0w <sthapliyal085@gmail.com>
2026-04-09 11:49:27 +0530

    "header"

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 "use client";
 
 import styles from "./style.module.scss";
+import Link from "next/link";
 
 export default function Header() {
     return (
         <header className={styles.header}>
-
+            <ul>
+                <Link href="/"><li>About</li></Link>
+                <Link href="/"><li>Contact</li></Link>
+            </ul>
         </header>
     );
 }
