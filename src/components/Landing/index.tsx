dee7bf9d4c4aade59db1e084083452738746ee9f
Saur0w <sthapliyal085@gmail.com>
2026-04-09 12:03:58 +0530

    "SplitLine"

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -11,16 +11,38 @@ gsap.registerPlugin(SplitText, useGSAP);
 
 export default function Landing() {
     const landingRef = useRef<HTMLDivElement>(null);
-    const textRef = useRef<HTMLParagraphElement>(null);
+    const textRef = useRef<HTMLDivElement>(null);
+
+    useGSAP(() => {
+        const split = new SplitText(textRef.current, {
+            type: "lines",
+            linesClass: "splitLine",
+            mask: "lines"
+        });
+
+        gsap.from(split.lines, {
+            yPercent: 110,
+            stagger: 0.08,
+            duration: 1,
+            ease: "power4.out",
+            delay: 0.3,
+        });
+
+        return () => split.revert();
+    }, {
+        scope: landingRef
+    })
     return (
         <section className={styles.landing} ref={landingRef}>
-            <p ref={textRef}>
-                Hi, I’m Astha Sati — currently pursuing my MBA.<br />
-                I’m passionate about business, problem-solving, <br />
-                and building ideas that create real-world impact. <br />
-                I’m always curious to learn, adapt, and grow through<br />
-                new experiences.
-            </p>
+            <div className={styles.para} ref={textRef}>
+                <p>
+                    Hi, I’m Astha Sati — currently pursuing my MBA.<br />
+                    I’m passionate about business, problem-solving, <br />
+                    and building ideas that create real-world impact. <br />
+                    I’m always curious to learn, adapt, and grow through<br />
+                    new experiences.
+                </p>
+            </div>
             <p>→<Link href="/">Let&#39;s Connect</Link></p>
 
             <footer className={styles.scroll}>Scroll</footer>
